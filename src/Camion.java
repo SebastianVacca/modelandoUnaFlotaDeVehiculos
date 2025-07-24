fb3647303cb28c79d2fc6987b6588abbf44d220d
Juan Vacca <clanbs11.12@gmail.com>
2025-07-23 20:51:03 -0500

    Add class Camion

---
+++ src/Camion.java
@@ -0,0 +1,23 @@
+public class Camion extends Vehiculo{
+    private Double capacidadCarga;
+
+    public Camion(String marca, String modelo, Integer anio, Double capacidadCarga) {
+        super(marca, modelo, anio);
+        this.capacidadCarga = capacidadCarga;
+    }
+
+    public Double getCapacidadCarga(){
+        return capacidadCarga;
+    }
+
+    @Override
+    public void mover() {
+        System.out.println("El camión está cargado y se está moviendo");
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.printf("Capacidad: %.1f%n", this.getCapacidadCarga());
+    }
+}
